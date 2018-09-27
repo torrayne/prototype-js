2bb3561b9bbf5ec972a538ece6ead6b04a2308ae
Daniel Atwood <daniel@atwood.io>
2018-09-27 16:01:44 -0400

    Condesned events.js

--- events.js
+++ events.js
@@ -1,14 +1,11 @@
-Node.prototype.on = function (trigger, handler, capture = false) {
-  addEventListener(trigger, handler.bind(this), capture)
-}
+Node.prototype.on = addEventListener
+Node.prototype.off = removeEventListener
 
 NodeList.prototype.on = function (trigger, handler, capture = false) {
   for (element in this) {
     element.on(trigger, handler.bind(this), capture)
   }
 }
-
-Node.prototype.off = removeEventListener
 
 NodeList.prototype.off = function (trigger, handler, capture = false) {
   for (element in this) {
