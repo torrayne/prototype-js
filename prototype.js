5f48069aa2bb9bcce92a963ed4b4614521b9b71b
Daniel Atwood <daniel@atwood.io>
2018-09-27 17:36:17 -0400

    Minified prototype

--- prototype.js
+++ prototype.js
@@ -23,6 +23,7 @@ NodeList.prototype.off = function (trigger, handler, capture = false) {
     element.off(trigger, handler, capture)
   }
 }
+
 function querySelector(query, simplify = true) {
   elements = document.querySelectorAll(query)
   if (elements.length == 1 && simplify) return elements[0]
