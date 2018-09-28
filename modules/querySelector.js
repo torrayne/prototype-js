30cb0cc63aa0ba23766176ee707f48fe34964c4d
Daniel Atwood <daniel@atwood.io>
2018-09-28 11:00:15 -0400

    Changed select function and created querySelector and querySelectorAll consts

--- modules/querySelector.js
+++ modules/querySelector.js
@@ -1,4 +1,7 @@
-function querySelector(query, simplify = true) {
+const querySelector = document.querySelector
+const querySelectorAll = document.querySelectorAll
+
+function select(query, simplify = true) {
   elements = document.querySelectorAll(query)
   if (elements.length == 1 && simplify) return elements[0]
   return elements
