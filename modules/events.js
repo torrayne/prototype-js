e5fe3d4dee89ceb239eb8597c66519ea7aaf1945
Daniel Atwood <daniel@atwood.io>
2018-10-11 22:08:54 -0400

    Updated event.js with spread and rest syntax

--- modules/events.js
+++ modules/events.js
@@ -1,12 +1,10 @@
 Node.prototype.on = addEventListener
 Node.prototype.off = removeEventListener
 
-NodeList.prototype.on = function (trigger, handler, capture = false) {
-  Array.prototype.map.call(this, node => node.on(trigger, handler, capture))
+NodeList.prototype.on = function (...atts) {
+  Array.prototype.map.call(this, node => node.on(...atts))
 }
 
-NodeList.prototype.off = function (trigger, handler, capture = false) {
-  NodeList.prototype.on = function (trigger, handler, capture = false) {
-    Array.prototype.map.call(this, node => node.off(trigger, handler, capture))
-  }
+NodeList.prototype.off = function (...atts) {
+    Array.prototype.map.call(this, node => node.off(...atts))
 }
