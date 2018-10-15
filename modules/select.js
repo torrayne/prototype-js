d9dd6e990924b5782d6a1e059b344c527ffd0b2f
Daniel Atwood <daniel@atwood.io>
2018-10-15 15:05:40 -0400

    Moved select to Node prototype

--- modules/select.js
+++ modules/select.js
@@ -1,5 +1,8 @@
-function select(query, simplify = true) {
-  const elements = document.querySelectorAll(query)
+Node.prototype.select = function (query, simplify = true) {
+	console.log(this)
+  const elements = this.querySelectorAll(query)
   if (elements.length == 1 && simplify) return elements[0]
   return elements
 }
+
+const select = (...atts) => document.select(...atts)
