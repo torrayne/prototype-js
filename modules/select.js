e6920cc82b16cccdc0f75380f9e2eb63acbb2eb5
Daniel Atwood <daniel@atwood.io>
2018-10-15 17:03:11 -0400

    Removed `console.log` from select.js

--- modules/select.js
+++ modules/select.js
@@ -1,5 +1,4 @@
 Node.prototype.select = function (query, simplify = true) {
-	console.log(this)
   const elements = this.querySelectorAll(query)
   if (elements.length == 1 && simplify) return elements[0]
   return elements
