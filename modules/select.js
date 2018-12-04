717be27aadb706ba4a8613692548e4e73ae23ea2
Daniel Atwood <daniel@atwood.io>
2018-12-04 15:03:06 -0500

    Removed limit range

--- modules/select.js
+++ modules/select.js
@@ -4,12 +4,6 @@ Node.prototype.select = function (query, limit = true) {
 	if (limit === true) {
 		if (elements.length == 0) return null
 		if (elements.length == 1) return elements[0]
-		return elements
-	}
-
-	if (typeof (limit) == 'number') {
-		if (limit == 1) return elements[0]
-		return Array.from(elements).slice(0, limit)
 	}
 
 	return elements
