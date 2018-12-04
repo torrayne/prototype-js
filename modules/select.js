cc36aba1a31f8ace9fd1710db6b0313412df8c1d
Daniel Atwood <daniel@atwood.io>
2018-12-04 15:03:34 -0500

    Limit can be truthy instead of boolean

--- modules/select.js
+++ modules/select.js
@@ -1,7 +1,7 @@
 Node.prototype.select = function (query, limit = true) {
 	let elements = this.querySelectorAll(query)
 
-	if (limit === true) {
+	if (limit == true) {
 		if (elements.length == 0) return null
 		if (elements.length == 1) return elements[0]
 	}
