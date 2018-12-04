15dd0a0a3b2a92eb961aa2c493c6a62a6f3299f1
Daniel Atwood <daniel@atwood.io>
2018-12-04 12:57:57 -0500

    Testing auto minifier

--- modules/elements.js
+++ modules/elements.js
@@ -1,6 +1,4 @@
-const createElement = (tagName, atts, ...rest) => Object.assign(
-	document.createElement(tagName, ...rest), atts
-)
+const createElement = (tagName, atts, ...rest) => Object.assign(document.createElement(tagName, ...rest), atts)
 
 Node.prototype.appendBefore = function (element) {
 	element.parentNode.insertBefore(this, element)
